e1dee11d455fb5b64694898384e7a6a290b66792
haewook Lee <atolow@gmail.com>
2025-04-17 16:30:38 +0900

    start

---
+++ src/test/java/example/com/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package example.com.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
