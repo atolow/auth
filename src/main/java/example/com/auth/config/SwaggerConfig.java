ced3b4f5481f2b1256e09903741a4f3fb0f6889d
haewook Lee <atolow@gmail.com>
2025-04-19 17:29:45 +0900

    feat:배포 완료

--- src/main/java/example/com/auth/config/SwaggerConfig.java
+++ src/main/java/example/com/auth/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Auth API 문서")
-                        .description("회원가입, 로그인, 권한 부여 등 인증 관련 API 문서")
+                        .description("회원가입, 로그인, 권한 부여, 로그아웃 등 인증 관련 API 문서")
                         .version("v1.0.0"));
     }
 }
