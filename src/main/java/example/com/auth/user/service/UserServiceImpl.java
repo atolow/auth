c64ca5684b1096ffd7a0220a6b54b04664b53484
hawwokii <hawwokii@naver.com>
2025-04-20 18:57:50 +0900

    배포 후 시간이 지나면 인스턴스 멈춤

--- src/main/java/example/com/auth/user/service/UserServiceImpl.java
+++ src/main/java/example/com/auth/user/service/UserServiceImpl.java
@@ -62,6 +62,7 @@ public class UserServiceImpl implements UserService {
         return UserResponseDto.from(user);
     }
 
+    @Override
     public LoginResponseDto login(LoginRequestDto request) {
         User user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(() -> new IllegalArgumentException("아이디 또는 비밀번호가 올바르지 않습니다."));
--- src/main/java/example/com/auth/utils/UserUtils.java
+++ src/main/java/example/com/auth/utils/UserUtils.java
@@ -9,7 +9,7 @@ public class UserUtils {
 
     public static User getUser(UserDetails userDetails) {
         if (userDetails == null) {
-            throw new UnauthorizedException("로그인이 필요합니다.");
+            throw new UnauthorizedException("아이디가 존재하지 않습니다.");
         }
 
         return ((UserDetailsImp) userDetails).getUser(); // ✅ 형변환은 유지
